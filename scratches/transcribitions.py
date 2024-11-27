45283dc2445aa27412a53eb406e8f8dded0fd7f3
Igor Makarov <realneiro@gmail.com>
2024-11-27 20:06:35 +0300

    4 texts for RAG

--- scratches/transcribitions.py
+++ scratches/transcribitions.py
@@ -2,11 +2,11 @@ import whisper
 import os
 
 
-audio = whisper.load_audio("rurik_.mp3")
+audio = whisper.load_audio("Varyag_3.mp3")
 
 whisper_model = whisper.load_model("turbo", device="cuda")
-result = whisper_model.transcribe(audio, language="Russian", fp16=False, verbose=True)["text"].strip()
+result = whisper_model.transcribe(audio, language="Russian", fp16=False, verbose=False)["text"].strip()
 
 
-with open("transcription.txt", "w") as file:
+with open("varyag_3.txt", "w", encoding="utf-8") as file:
     file.write(result)
