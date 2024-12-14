7938ef765ec0f8cf14507f2bd662e6473d4c891f
Pavel Sokolov <click315@mail.ru>
2024-12-14 10:02:06 +0300

    Rag integration Раг подключен к приложению Добавлена кнопка перезапуска бота в меню

--- app/routes/ml.py
+++ app/routes/ml.py
@@ -3,14 +3,19 @@ from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
 from utils.states import ProcessLLMStates
+from services.rag_service import RAGService
+
+rag_service = RAGService()
+
+collection_name = "russo_japanese_war"
 
 router = Router()
 
 
 @router.message(ProcessLLMStates.waitForText)
 async def request_generate(message: Message, state: FSMContext):
-    user_text = message.text
-    
-    answer = "вот тебе умный развернутый ответ"
+    query = message.text
+
+    answer = rag_service.generate_answer(query, collection_name)
 
     await message.answer(answer)
