5f75e39a59b05a32ce2991f4d23a93f1ad9c5038
Igor Makarov <realneiro@gmail.com>
2024-12-14 22:30:13 +0300

    best system prompt evar

--- app/routes/commands.py
+++ app/routes/commands.py
@@ -12,14 +12,14 @@ router = Router()
 @router.message(Command("start"))
 async def cmd_start(message: Message, state: FSMContext):
     msg_1 = """
-    Привет!
-    Я своего рода эксперт в русско-японской войне.
-    Можем обсудить с тобой эту тему.
-    """
-    msg_2 = "Задавай свои вопросы, а я расскажу тебе все, что знаю"
+Здравия желаю, Ваше Превосходительство!
+
+Я - жалкая тень великого адмирала Степана Осиповича Макарова.
+
+Но я могу ответить на Ваши вопросы по Русско-Японской войне 1904-05гг.
+"""
 
     logging.info(f"State set to: {await state.get_state()}")
     await message.answer(msg_1)
-    await message.answer(msg_2)
     await state.set_state(ProcessLLMStates.waitForText)
     logging.info(f"State set to: {await state.get_state()}")
